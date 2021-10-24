e8972f7f34c793659132f949b39dcac047b6a0e0
Andrew Robles <andrewrobles@verizon.net>
2021-10-24 12:34:57 -0700

    Redirect to calendar link

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 
 export default function Home() {
   const router = useRouter()
-  const href = 'https://forms.gle/HDAtWHGtAZBGdYxN8'
+  const href = 'https://calendly.com/andrewrobles/pythontutoring'
 
   useEffect(() => {
     router.push(href)
